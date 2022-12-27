eaf9d85d6d7468c724c82d9d2611ab27c827947b
Maksym Nenashev <57881489+maks-nenashev@users.noreply.github.com>
2022-12-27 14:50:54 +0100

    dla znachenija po umolchaniju

--- app1.rb
+++ app1.rb
@@ -1,7 +1,32 @@
-
-
- #Konstrukcija  "or" "ili" esli x ne inicialeziruetsja ili--false to znachenie budet "2"
+#Konstrukcija  "or" "ili" esli x ne inicialeziruetsja ili--false to znachenie budet "2"
 x = x||2 
+         # Toze samoe
+x|| = y
+ # Eto ispolzuetsa dla ustanowki znachenija po umolchaniju
 
+ #Срабатывает первое условие, если не срабатывает, берёт второе условие:
 
-
+ nil || 4 #=> 4
+ false || 2 #=> 2
+ 123 || 2 #=> 123
+ 
+ x = x || 4
+ x = false
+ x = x || 2
+ puts x
+ #=> 2
+ 
+ x = true
+ x = x || 2
+ puts x
+ #=> true
+ 
+ x = 5
+ x = x || 2 # это мы можем заменить на x ||= 2
+ puts x
+ #=> 5
+ 
+ x = 5
+ x ||= 2
+ puts x
+ #=> 5
